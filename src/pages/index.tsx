a055a63f16f3abc70faee45ab54e2a838e8f7698
Jed Hastwell <jed@whistlefire.com>
2023-07-18 13:52:09 +0200

    Pre-fetch about page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <Link
       className="absolute flex h-full w-full cursor-pointer flex-row items-center justify-center bg-burnt-yellow"
       href="/about"
+      prefetch={true}
     >
       <div className="drop-shadow-orange">
         <div className="relative block h-[200px] w-[200px] bg-[#3D3D3D] [mask:url('/iconic.svg')] sm:h-[250px] sm:w-[250px]"></div>
