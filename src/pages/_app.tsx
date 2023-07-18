36ba6a47b0298803bb86d64f8dc2a2ccc50c2229
Jed Hastwell <jed@whistlefire.com>
2023-07-18 13:51:20 +0200

    Re-add page title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,15 @@ import '@/styles/globals.css'
 import Card from '@/components/card'
 import type { AppProps } from 'next/app'
 import { faktPro } from '@/styles/fonts'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <div className={`${faktPro.className} [perspective:1200px]`}>
+      <Head>
+        <title>Jed Hastwell</title>
+        <meta name="robots" content="noindex"></meta>
+      </Head>
       <Card>
         <Component {...pageProps} key={router.asPath} />
       </Card>
